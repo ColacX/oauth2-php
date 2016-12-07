fa901d9ad3355ef44ef26ac2c510f51733ed56af
ColacX <DanielZheng1989@gmail.com>
2016-12-07 14:37:16 +0100

    changed directory

--- custom_deploy.cmd
+++ custom_deploy.cmd
@@ -1,5 +1,5 @@
 echo Downloading Composer
 curl -sS https://getcomposer.org/installer | php
 
-echo Invoking composer install in deployment directory $DEPLOYMENT_TARGET
-php -d extension=php_intl.dll %DEPLOYMENT_TARGET%/oauth2-azure-1.1.1/composer.json install -v --prefer-dist --no-dev --optimize-autoloader --no-interaction
+echo Invoking composer install in directory %DEPLOYMENT_SOURCE%
+php -d extension=php_intl.dll %DEPLOYMENT_SOURCE%/composer.phar install -v --prefer-dist --no-dev --optimize-autoloader --no-interaction
