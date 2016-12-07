c75811d22b7f59470ae7f5c43d3824944288754f
ColacX <DanielZheng1989@gmail.com>
2016-12-07 17:20:06 +0100

    login.php

--- test.php
+++ test.php
@@ -1,15 +1,1 @@
-<?php
-echo "PHP START\n";
-
-$dir    = './vendor';
-$files1 = scandir($dir);
-
-print_r($files1);
-
-$dirs = array_filter(glob('*'));
-print_r($dirs);
-
-echo file_get_contents("./vendor/autoload.php");
-
-echo "PHP FINISH\n";
-?>
+<a href="./login.php">login.php</a>
