3fcf144310f29e332fe75eed977e53deaef86834
ColacX <DanielZheng1989@gmail.com>
2016-12-07 16:36:47 +0100

    test.php

--- test.php
+++ test.php
@@ -7,7 +7,7 @@ for ($x = 0; $x <= 10; $x++) {
     echo "The number is: $x <br>";
 }
 
-echo file_get_contents( "vendor/autoload.php" );
+echo file_get_contents( "./vendor/autoload.php" );
 
 echo "wtf"
 ?>
