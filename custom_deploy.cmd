a544eb5fd8cc53adab8455408571b800d852b3ca
ColacX <DanielZheng1989@gmail.com>
2016-12-07 15:09:11 +0100

    print contents

--- custom_deploy.cmd
+++ custom_deploy.cmd
@@ -3,3 +3,9 @@ curl -sS https://getcomposer.org/installer | php
 
 echo Invoking composer install in directory %DEPLOYMENT_SOURCE%
 php -d extension=php_intl.dll %DEPLOYMENT_SOURCE%/composer.phar install -v --prefer-dist --no-dev --optimize-autoloader --no-interaction
+
+echo %DEPLOYMENT_SOURCE%
+dir %DEPLOYMENT_SOURCE%
+
+echo %DEPLOYMENT_TARGET%
+dir %DEPLOYMENT_TARGET%
