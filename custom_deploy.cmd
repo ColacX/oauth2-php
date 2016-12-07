51b2e8cde5fa68fdedfa903183664cbfb738144d
ColacX <DanielZheng1989@gmail.com>
2016-12-07 15:32:27 +0100

    fix deploy script

--- custom_deploy.cmd
+++ custom_deploy.cmd
@@ -1,8 +1,17 @@
+echo Deleting contents of %DEPLOYMENT_TARGET%
+del /q destination\*
+for /d %x in (destination\*) do @rd /s /q "%x"
+
+echo Copy Contents from %DEPLOYMENT_SOURCE% to %DEPLOYMENT_TARGET%
+cp -a %DEPLOYMENT_SOURCE% %DEPLOYMENT_TARGET%
+
 echo Downloading Composer
+cd %DEPLOYMENT_TARGET%
 curl -sS https://getcomposer.org/installer | php
 
 echo Invoking composer install in directory %DEPLOYMENT_SOURCE%
-php -d extension=php_intl.dll %DEPLOYMENT_SOURCE%/composer.phar install -v --prefer-dist --no-dev --optimize-autoloader --no-interaction
+cd %DEPLOYMENT_TARGET%
+php -d extension=php_intl.dll composer.phar install -v --prefer-dist --no-dev --optimize-autoloader --no-interaction
 
 echo %DEPLOYMENT_SOURCE%
 dir %DEPLOYMENT_SOURCE%
