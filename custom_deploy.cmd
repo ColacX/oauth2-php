775fd607792dfe20c1b8952ef28f250f81b3efb7
ColacX <DanielZheng1989@gmail.com>
2016-12-07 14:16:50 +0100

    try again

--- custom_deploy.cmd
+++ custom_deploy.cmd
@@ -2,4 +2,4 @@ echo Downloading Composer
 curl -sS https://getcomposer.org/installer | php
 
 echo Invoking composer install in deployment directory $DEPLOYMENT_TARGET
-php -d extension=php_intl.dll %DEPLOYMENT_TARGET%/composer.phar install -v --prefer-dist --no-dev --optimize-autoloader --no-interaction
+php -d extension=php_intl.dll %DEPLOYMENT_TARGET%/oauth2-client-1.4.2/composer.json install -v --prefer-dist --no-dev --optimize-autoloader --no-interaction
