89660c1bab0740637bbe1e4fc089c785a88ae053
ColacX <DanielZheng1989@gmail.com>
2016-12-07 16:21:27 +0100

    again

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require __DIR__ . '/vendor/autoload.php';
+require 'vendor/autoload.php';
 
 echo "Hello World!" . " php " . " works ";
 
